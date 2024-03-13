c8e032cf0d76a4a33455b126cc5b8ef9cb03c997
Ahmad Najib <eng.ahmad.najeeb17@gmail.com>
2024-03-14 00:46:14 +0300

    023 Throttling the API

--- app/Http/Controllers/Api/AttendeeController.php
+++ app/Http/Controllers/Api/AttendeeController.php
@@ -16,6 +16,7 @@ class AttendeeController extends Controller
     public function __construct()
     {
         $this->middleware('auth:sanctum')->except(['index', 'show', 'update']);
+        $this->middleware('throttle:api')->only(['store', 'destroy']);
         $this->authorizeResource(Attendee::class, 'attendee');
     }
     public function index(Event $event)
@@ -33,7 +34,7 @@ class AttendeeController extends Controller
     {
         $attendee = $this->loadRelationships(
             $event->attendees()->create([
-                'user_id' => 1
+                'user_id' => $request->user()->id
             ])
         );
         return new AttendeeResource($attendee);
@@ -62,7 +63,6 @@ class AttendeeController extends Controller
      */
     public function destroy(Event $event, Attendee $attendee)
     {
-        // $this->authorize('delete-attendee', [$event, $attendee]);
         $attendee->delete();
         return response(status: 204);
     }
